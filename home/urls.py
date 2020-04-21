f03d72ff605a3beb05f8f813cc7505fa4d86b8af
itskaramaman <singhkaramjeetapple@gmail.com>
2020-04-22 01:33:21 +0530

    Edit, delete not done, pdf to be done

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<int:note_id>', views.note, name='note'),
+    path('<int:note_id>/', views.note, name='note'),
+    path('delete/<int:note_id>/', views.delete_note, name='delete-note'),
+    path('edit/<int:note_id>/', views.edit_note, name='edit-note'),
+    path('pdf/<int:note_id>/', views.save_pdf, name='pdf-note'),
 ]
--- home/views.py
+++ home/views.py
@@ -27,3 +27,36 @@ def note(request, note_id):
     selected_note = get_object_or_404(Note, pk=note_id)
     context = {'note': selected_note}
     return render(request, 'home/note.html', context)
+
+
+def delete_note(request, note_id):
+    selected_note = get_object_or_404(Note, pk=note_id)
+    selected_note.delete()
+    return redirect('home')
+
+
+def edit_note(request, note_id):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['editor']
+        note = Note.objects.filter(pk=note_id).update(
+            title=title, content=content)
+
+        return redirect('home')
+
+    selected_note = get_object_or_404(Note, pk=note_id)
+    title = selected_note.title
+    content = selected_note.content
+    context = {
+        'title': title,
+        'content': content,
+        'note_id': note_id
+    }
+    return render(request, 'home/edit_note.html', context)
+
+
+def save_pdf(request, note_id):
+    note = get_object_or_404(Note, note_id)
+    title = note.title
+    content = note.content
+    pass
